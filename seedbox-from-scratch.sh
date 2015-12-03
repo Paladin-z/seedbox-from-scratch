983185204e0889684869da21571156fc3b9673b9
Paladin-z <paladinz@inbox.com>
2015-12-03 14:14:26 +0000

    Update seedbox-from-scratch.sh

--- seedbox-from-scratch.sh
+++ seedbox-from-scratch.sh
@@ -272,7 +272,7 @@ apt-get --yes update
 apt-get --yes install whois sudo makepasswd git
 
 rm -f -r /etc/seedbox-from-scratch
-git clone -b v$SBFSCURRENTVERSION1 https://github.com/Notos/seedbox-from-scratch.git /etc/seedbox-from-scratch
+git clone -b v$SBFSCURRENTVERSION1 https://github.com/Paladin-z/seedbox-from-scratch.git /etc/seedbox-from-scratch
 mkdir -p cd /etc/seedbox-from-scratch/source
 mkdir -p cd /etc/seedbox-from-scratch/users
 
